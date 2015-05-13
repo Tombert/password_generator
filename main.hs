7f034269f5a302e239ef8e74af8e74b9f971c161
Thomas Gebert <thomasgebert@gmail.com>
2015-05-13 15:09:36 -0400

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,10 +1,16 @@
 module Main where
 import System.Entropy
+import qualified Data.ByteString as B
 import System.Environment
-import Data.ByteString.Base64 as B64
+import Codec.Binary.Base85 as B85
+
+toBase85 :: B.ByteString -> String
+toBase85 = B85.encode . B.unpack
 
 main :: IO ()
 main = do
         args <- getArgs
-        entropy <- getEntropy (read (args !! 0) :: Int)
-        print ( B64.encode entropy)
+        let length = read $ args !! 0 :: Int
+        entropy <- getEntropy length
+        let blah = toBase85 entropy
+        print blah
